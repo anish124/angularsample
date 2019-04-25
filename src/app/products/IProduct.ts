537482b1a1dfaf83843e357eb1b0b31f74c42b2e
anish <anish.g.reddy@gmail.com>
2019-04-25 07:34:30 +0530

    updations

---
+++ src/app/products/IProduct.ts
@@ -0,0 +1,24 @@
+export class IProduct {
+
+    constructor(
+
+       
+ 
+
+        public productId: string,
+
+        public productName: string,
+
+        public releaseDate: string,
+
+        public description: string,
+
+        public price: string,
+
+        public starRating: string,
+
+  public imageUrl: string
+
+    ) { }
+
+}
